47a04d0a222b06dfdc96906fa717ce8ee641aed9
barinalex <mr.barrri@gmail.com>
2022-04-22 13:28:03 +0200

    observations from realsense incorporated

--- src/agentdriver.py
+++ src/agentdriver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import rospy
 from buggycontrol.msg import ActionsStamped
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Quaternion, Vector3
 from std_msgs.msg import Header
 from threading import Lock
 from agent import Agent
@@ -8,6 +10,42 @@ from queuebuffer import QueueBuffer
 from waypointer import Waypointer
 from state import State
 import numpy as np
+import tf
+import tf2_ros
+import time
+
+
+def get_static_tf(source_frame, target_frame):
+    """
+    :param source_frame: source frame
+    :param target_frame: target frame
+    :return: static transformation from source to target frame
+    """
+    tfbuffer = tf2_ros.Buffer()
+    tf2_ros.TransformListener(tfbuffer)
+    for i in range(100):
+        print("lookup", i)
+        try:
+            trans = tfbuffer.lookup_transform(target_frame,
+                                              source_frame,
+                                              rospy.Time(0),
+                                              rospy.Duration(0))
+            return trans
+        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException) as err:
+            rospy.logwarn_throttle(1, "ros_utils tf lookup could not lookup tf: {}".format(err))
+            time.sleep(0.2)
+            continue
+
+
+def rotate_vector_by_quat(v: Vector3, q: Quaternion):
+    """
+    :param v: vector
+    :param q: quaternion
+    :return: rotated vector by quaternion
+    """
+    qm = tf.transformations.quaternion_matrix([q.x, q.y, q.z, q.w])[:3, :3]
+    new_v = np.matmul(qm, np.array([v.x, v.y, v.z]))
+    return Vector3(x=new_v[0], y=new_v[1], z=new_v[2])
 
 
 class AgentDriver:
@@ -30,8 +68,11 @@ class AgentDriver:
         self.action = np.array([-1, 0])
         rospy.init_node("agent")
         rospy.Subscriber("joy", ActionsStamped, self.callback)
+        rospy.Subscriber("/camera/odom/sample", Odometry, self.odomcallback)
+        self.bl2rs = get_static_tf("odom", "camera_odom_frame")
         pub = rospy.Publisher("actions", ActionsStamped, queue_size=10)
         self.actlock = Lock()
+        self.odomlock = Lock()
         self.msg = None
         rate = rospy.Rate(200)
         while not rospy.is_shutdown():
@@ -42,7 +83,8 @@ class AgentDriver:
             rate.sleep()
 
     def updatestate(self):
-        self.state.set(vel=self.lin, ang=self.ang)
+        with self.odomlock:
+            self.state.set(vel=self.lin, ang=self.ang)
         self.state.update_pos()
         self.state.update_orn()
 
@@ -76,6 +118,27 @@ class AgentDriver:
             msg.header = Header(stamp=rospy.Time.now(), frame_id="base_link")
             self.msg.throttle, self.msg.turn = self.action[0], self.action[1]
             self.msg.buttonA, self.msg.buttonB = msg.buttons[0], msg.buttons[1]
+
+    def rotate_twist(self, odom_msg):
+        """
+        Transform the twist in the odom message
+        :param odom_msg: odometry message
+        :return: transformed odometry message
+        """
+        odom_msg.twist.twist.linear = rotate_vector_by_quat(odom_msg.twist.twist.linear,
+                                                            self.bl2rs.transform.rotation)
+        odom_msg.twist.twist.angular = rotate_vector_by_quat(odom_msg.twist.twist.angular,
+                                                             self.bl2rs.transform.rotation)
+        return odom_msg
+
+    def odomcallback(self, odom: Odometry):
+        """
+        :param odom: message containing odometry data
+        """
+        odom = self.rotate_twist(odom)
+        with self.odomlock:
+            self.lin = np.asarray([odom.twist.twist.linear.x, odom.twist.twist.linear.y, odom.twist.twist.linear.z])
+            self.ang = np.asarray([odom.twist.twist.angular.x, odom.twist.twist.angular.y, odom.twist.twist.angular.z])
 
 
 if __name__ == "__main__":
