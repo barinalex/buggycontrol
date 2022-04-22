6eaa584da3381687f88945d87bfcd7c8de6a3ca3
barinalex <mr.barrri@gmail.com>
2022-04-22 09:59:21 +0200

    mutex when reading actions from joystick

--- src/joyconverter.py
+++ src/joyconverter.py
@@ -2,6 +2,7 @@
 import rospy
 from buggycontrol.msg import Actions
 from sensor_msgs.msg import Joy
+from threading import Lock
 import numpy as np
 
 
@@ -10,31 +11,24 @@ class JoyConverter:
         rospy.init_node("joyconverter")
         rospy.Subscriber("joy", Joy, self.callback)
         pub = rospy.Publisher("actions", Actions, queue_size=10)
-        self.throttle = -1
-        self.turn = 0
-        self.a = False
-        self.b = False
-        self.update = False
-        self.data = (-1, 0, False, False)
+        self.actlock = Lock()
+        self.msg = None
         rate = rospy.Rate(200)
         while not rospy.is_shutdown():
-            if self.update:
-                self.throttle, self.turn, self.a, self.b = self.data
-                self.update = False
-            msg = Actions()
-            msg.throttle, msg.turn = self.throttle, self.turn
-            msg.buttonA, msg.buttonB = self.a, self.b
-            pub.publish(msg)
+            with self.actlock:
+                if self.msg:
+                    pub.publish(self.msg)
+                    self.msg = None
             rate.sleep()
 
     def callback(self, msg):
         """
         :param msg: raw input from joystick
         """
-        throttle, turn = msg.axes[1], msg.axes[3]
-        a, b = msg.buttons[0], msg.buttons[1]
-        self.data = (throttle, turn, a, b)
-        self.update = True
+        with self.actlock:
+            self.msg = Actions()
+            self.msg.throttle, self.msg.turn = msg.axes[1], msg.axes[3]
+            self.msg.buttonA, self.msg.buttonB = msg.buttons[0], msg.buttons[1]
 
 
 if __name__ == "__main__":
